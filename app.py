e2f2058055b236a8e738df86d8cbe7fa79090233
bhupesh-6124 <wankhadebhupesh@gmail.com>
2025-01-23 13:39:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, session, url_for
 from flask_sqlalchemy import SQLAlchemy
-import joblib
+from sklearn.externals import joblib
 import numpy as np
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Load the trained model
-model = joblib.load('diabetes_model.pkl')
+mjoblib.dump(model, 'diabetes_model.pkl')
 
 
 # Initialize the database
